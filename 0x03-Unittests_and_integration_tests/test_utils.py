8091d06e2e4f6089bb2df2d41a9577c8783ef49a
sossesam <sossesam@gmail.com>
2025-09-21 20:13:59 +0100

    created my first decorator

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,7 @@
 from parameterized import parameterized
 import unittest
 from unittest.mock import patch, Mock
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 class TestAccessNestedMap(unittest.TestCase):
 
@@ -48,4 +48,30 @@ class TestGetJson(unittest.TestCase):
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(result, test_payload)
         
-        
+class TestMemoize(unittest.TestCase) :
+
+    def test_memoize(self):
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+                
+        with patch.object(TestClass, "a_method", return_value=42) as mock_method:
+            obj = TestClass()
+
+            # Call twice
+            result1 = obj.a_property
+            result2 = obj.a_property
+
+            # Check results are correct
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+
+            # Ensure a_method was only called once
+            mock_method.assert_called_once()
+            
+                
