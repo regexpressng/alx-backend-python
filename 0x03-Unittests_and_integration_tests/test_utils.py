ea03fcc0f3c083c0ffb4b6d522373afdb1e154ec
sossesam <sossesam@gmail.com>
2025-09-21 19:09:05 +0100

    created my first decorator

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,8 @@
 
 from parameterized import parameterized
 import unittest
-from utils import access_nested_map
+from unittest.mock import patch, Mock
+from utils import access_nested_map, get_json
 
 class TestAccessNestedMap(unittest.TestCase):
 
@@ -21,3 +22,30 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, nested_map, path):
         with self.assertRaises(KeyError) :
             access_nested_map(nested_map, path)
+
+   
+    
+class TestGetJson(unittest.TestCase):
+    """
+    Tests for the get_json function from utils.
+    """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch("utils.requests.get")
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """
+        Test get_json function with mocked requests.get."""
+        mock_response = Mock()
+        
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        result = get_json(test_url)
+
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
+        
+        
