2f0bac15d53fd8aa85d3aad716064ba39fd79b19
sossesam <sossesam@gmail.com>
2025-09-21 15:48:37 +0100

    created my first decorator

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from parameterized import parameterized
 import unittest
 from utils import access_nested_map
@@ -7,7 +9,14 @@ class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 3}}, ("a", "b"), 2),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({}, ("a"), 1),
+        ({"a": 1}, ("a", "b"), 3)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    
+    def test_access_nested_map_exception(self):
+        with self.assertRaises(KeyError) :
+            access_nested_map({}, "a")
