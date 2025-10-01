68953abdf548480892dcbce7716e6d028565642f
sossesam <sossesam@gmail.com>
2025-10-01 13:47:41 +0100

    created my first decorator

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -14,8 +14,7 @@ class TestGithubOrgClient(unittest.TestCase):
     This class contains unit tests that verify the correct behavior of the
     GithubOrgClient by mocking API calls and checking the resulting output.
     """
-    
-   
+
     @parameterized.expand([("google", {"id": 7697149}),
                            ("abc", {"message": "Not Found"})])
     @patch("client.get_json")
@@ -25,26 +24,27 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_response.return_value = payload
         mock_get_json.return_value = payload
 
-
         client = GithubOrgClient(org)
         result = client.org
 
-        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org}")
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org}")
         self.assertEqual(result, payload)
 
     @parameterized.expand([
-    ("google", {"repos_url": "https://api.github.com/orgs/google/repos"}),
-    ("abc", {"repos_url": "https://api.github.com/orgs/abc/repos"})])
-
+        ("google", {"repos_url": "https://api.github.com/orgs/google/repos"}),
+        ("abc", {"repos_url": "https://api.github.com/orgs/abc/repos"})])
     def test_public_repos_url(self, org, test_payload):
         """
         Tests  that it returns public repos
         """
-        with patch.object(GithubOrgClient, "org", new_callable=PropertyMock) as mock_org:
+        with patch.object(GithubOrgClient,
+                          "org", new_callable=PropertyMock) as mock_org:
             mock_org.return_value = test_payload
 
             client = GithubOrgClient(org)
-            self.assertEqual(client._public_repos_url, test_payload["repos_url"])
+            self.assertEqual(client._public_repos_url,
+                             test_payload["repos_url"])
             mock_org.assert_called_once()
 
     @patch("client.get_json")
@@ -54,14 +54,12 @@ class TestGithubOrgClient(unittest.TestCase):
         """
         mock_get_json.return_value = "https://api.github.com/orgs/google/repos"
 
-        with patch.object(GithubOrgClient, "org", new_callable=PropertyMock) as mock_org:
+        with patch.object(GithubOrgClient,
+                          "org", new_callable=PropertyMock) as mock_org:
             mock_org.return_value = "https://api.github.com/orgs/google/repos"
-        
-        mock_get_json.assert_called_once_with("https://api.github.com/orgs/google/repos")
 
-        
-
-
+        mock_get_json.assert_called_once_with(
+            "https://api.github.com/orgs/google/repos")
 
     @parameterized.expand([
         ({"license": {"key": "my_license"}}, "my_license", True),
