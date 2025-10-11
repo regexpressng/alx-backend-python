9628a5564da3e637e7842c25820c44dd79e7876f
sossesam <sossesam@gmail.com>
2025-10-11 08:42:28 +0100

    messaging app

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -68,6 +68,20 @@ TEMPLATES = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+        # 'rest_framework.permissions.AllowAny', # Example of another permission class
+        # 'rest_framework.permissions.IsAdminUser', # Example of another permission class
+    ),
+    # ... other DRF settings if needed ...
+}
+
 WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
 
