58c9e8434a9944d8a56bb52bf0287826a1f39be1
sossesam <sossesam@gmail.com>
2025-11-22 11:42:31 +0100

    Added viewset

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chats.urls')),  # 👈 include your app’s routes under /api/
+    path('api-auth/', include('rest_framework.urls')),
 ]
