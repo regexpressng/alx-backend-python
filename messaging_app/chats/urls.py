a1eddc0cb3362ee54e397675cebf51cfd6bc3930
sossesam <sossesam@gmail.com>
2025-11-22 11:40:59 +0100

    Added viewset

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -5,8 +5,12 @@ from .views import ConversationViewSet, MessageViewSet
 
 router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
-router.register(r'messages', MessageViewSet, basename='message')
+conversation_router = routers.NestedDefaultRouter(router, r'conversations', lookup='conversation')
+conversation_router.register(r'messages', MessageViewSet, basename='conversation-messages')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(conversation_router.urls)),
+    path('messages/', MessageViewSet.as_view(), name='message_list_create'),
+    path('conversations/', ConversationViewSet.as_view(), name='conversation_list'),
 ]
