d6f0a80a4f658819c0490590ccf74a3574043ecf
sossesam <sossesam@gmail.com>
2025-11-22 11:01:31 +0100

    Added viewset

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -12,42 +12,21 @@ from .serializers import (
 )
 
 
-class ConversationViewSet(viewsets.ModelViewSet):
+class ConversationViewSet(viewsets.ViewSet):
     """
     ViewSet for listing and creating conversations.
     """
-    queryset = Conversation.objects.prefetch_related('participants', 'messages__sender').all()
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_serializer_class(self):
-        # Use a different serializer for creation
-        if self.action == 'create':
-            return ConversationCreateSerializer
-        return ConversationSerializer
-
-    def perform_create(self, serializer):
-        # Automatically include the current user in the conversation
-        conversation = serializer.save()
-        conversation.participants.add(self.request.user)
-        conversation.save()
-
-    @action(detail=True, methods=['get'], url_path='messages')
-    def list_messages(self, request, pk=None):
-        """List all messages in a given conversation."""
-        conversation = self.get_object()
-        messages = conversation.messages.select_related('sender').all()
-        serializer = MessageSerializer(messages, many=True)
+    def list(self):
+        queryset = Conversation.objects.all()
+        serializer =  ConversationSerializer(queryset)
         return Response(serializer.data)
+    
 
-
-class MessageViewSet(viewsets.ModelViewSet):
+class MessageViewSet(viewsets.ViewSet):
     """
-    ViewSet for sending and listing messages.
+        Viewset to retrieve messages
     """
-    queryset = Message.objects.select_related('sender', 'conversation').all()
-    serializer_class = MessageSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def perform_create(self, serializer):
-        """Automatically set the sender to the current user."""
-        serializer.save(sender=self.request.user)
+    def retrieve(self, request, conversation_id=None):
+        queryset = Message.objects.filter( conversation__conversation_id=conversation_id)
+        serializer = MessageSerializer(queryset)
+        return Response(serializer)
