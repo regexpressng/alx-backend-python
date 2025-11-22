1557292e82cb6670e7c168561ec6c084cdec0497
sossesam <sossesam@gmail.com>
2025-11-22 11:27:08 +0100

    Added viewset

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets, permissions, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Conversation, Message
 from .serializers import (
