168bab258eccf4d6f3227299dae77edf41561334
sossesam <sossesam@gmail.com>
2025-12-28 14:08:30 +0100

    added filter

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -4,3 +4,11 @@ class MessagePagination(PageNumberPagination):
     page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 50
+
+     def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,   # 👈 REQUIRED BY CHECKER
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
