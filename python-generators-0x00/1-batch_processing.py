7676d5d9c366e2c38c87a7d31bb21c93b3b9ff9e
regexpressng <regexpressng@gmail.com>
2026-08-06 11:34:47 +0100

    Update 1-batch_processing.py

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,45 +1,42 @@
-import mysql.connector
-
-
-
+#!/usr/bin/python3
+import seed
+stream_users = __import__('0-stream_users')
 
 def stream_users_in_batches(batch_size):
-    connection = connection = mysql.connector.connect(
-        host = 'localhost',
-        user = 'alx_user',
-        password = 'asdffdsa',
-        database = 'ALX_prodev'
-    )
-   
-    cursor = connection.cursor(dictionary=True)
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor(dictionary=True, buffered=True)
+    cursor.execute("SELECT * FROM user_data;")
 
-    cursor.execute("SELECT * FROM user_data")
-
-    while True:
-        users = cursor.fetchmany(batch_size)
-        for user in users:
-            yield user
-
-        cursor.close()
-        connection.close()
 
     
-        
-
-        
+    try:
+        while True:
+            # Fetch the next batch of rows
+            user_batch = cursor.fetchmany(batch_size)
             
+            # cursor.fetchmany() returns an empty list [] when no more rows remain
+            if not user_batch:
+                break
+                
+            yield user_batch
 
-
+    finally:
+        # Clean up resources
+        cursor.close()
+        connection.close()
+    
+    
 
 def batch_processing(batch_size):
-    users = stream_users_in_batches(batch_size)
-    while True:
-        try:
-            user = next(users)
-            if user["age"] > 25:
+    user_batch_stream = stream_users_in_batches(batch_size)
+
+    try:
+        current_batch = next(user_batch_stream)
+        for user in current_batch:
+            if user.get("age") > 25:
                 print(user)
-
-        except StopIteration:
-            break
-
+    except StopIteration:
+        return
+        
+    
     
